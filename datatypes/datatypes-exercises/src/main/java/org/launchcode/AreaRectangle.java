7396f2740181ecbc2e85916efd4bd7baede47fbf
Esther Onchieku <estheronchieku@gmail.com>
2023-08-21 17:20:46 -0500

    First commmit. Examples and exercises modified.

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/AreaRectangle.java
@@ -0,0 +1,19 @@
+package org.launchcode;
+
+import java.util.Scanner;
+
+public class AreaRectangle {
+//    double length;
+//    double width;
+//    doube area;
+//
+//    public static void main(String[] args){
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter the length of the rectangle ");
+//        length = input.nextDouble();
+//        System.out.println("Enter the width of the rectangle ");
+//        width = input.nextDouble();
+//        area = (length * width);
+//
+//    }
+}
