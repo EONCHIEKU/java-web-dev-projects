3a4e26ef5589ed3640c2ac3ce7a7872e2614451a
Esther Onchieku <estheronchieku@gmail.com>
2023-08-23 18:29:37 -0500

    User Input Validation using Conditional Syntax

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -8,11 +8,27 @@ public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 // BASIC SOLUTION
-        System.out.println("Enter radius of the circle: ");
-        double radius = input.nextDouble();
-        double area = Circle.getArea(radius);
-        System.out.println("The area of circle with "+ radius + " is: " + area);
+//        System.out.println("Enter radius of the circle: ");
+//        double radius = input.nextDouble();
+//        double area = Circle.getArea(radius);
+//        System.out.println("The area of circle with "+ radius + " is: " + area);
 
+        //BONUS MISSION 1
+        System.out.println("Enter radius of the circle:");
+
+        if (input.hasNextDouble()){
+            double radius = input.nextDouble();
+
+            if (radius > 0){
+                double area = Circle.getArea(radius);
+                System.out.println("The area of circle with "+ radius + " is: " + area);
+            } else{
+                System.out.println("Sorry. The radius has to be a positive number.");
+            }
+        }else{
+            System.out.println("Sorry. That is not a valid number.");
+        }
+//BONUS MISSION 2 -
         input.close();
 
     }
