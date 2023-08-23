9ca939a40dbc7dc38413b80d00724ee461cbc721
Esther Onchieku <estheronchieku@gmail.com>
2023-08-23 18:02:47 -0500

    Area of Circle calculation using user input and different classes

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -7,11 +7,13 @@ import java.util.Scanner;
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+// BASIC SOLUTION
         System.out.println("Enter radius of the circle: ");
-        double userRadius = input.nextDouble();
-        double area = 3.14 * userRadius * userRadius;
-        System.out.println("The area of circle with "+ userRadius + " is: " + area);
+        double radius = input.nextDouble();
+        double area = Circle.getArea(radius);
+        System.out.println("The area of circle with "+ radius + " is: " + area);
+
+        input.close();
 
     }
 }
